5b24f62d371025ac6022a26c2013d19c18eadb5d
Abhishek shukla <abhishekshukla824@gmail.com>
2023-12-23 22:48:27 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/abhis3110/basics-jwt-go.git
+
+go 1.19
+
+require github.com/golang-jwt/jwt/v5 v5.2.0
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// we will implement these handlers in the next sections
+	http.HandleFunc("/signin", Signin)
+	http.HandleFunc("/welcome", Welcome)
+	http.HandleFunc("/refresh", Refresh)
+	http.HandleFunc("/logout", Logout)
+
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
